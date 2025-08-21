035b9de721b2263deb7e7eee21254c2c159c00df
Aakash Pathak <aakashpathakece063@gmail.com>
2025-08-22 01:01:25 +0530

    added variables and datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 144234
+let accountEmail = "aakashpathak.com"
+var accPassword = "12343"
+accountCity = "jaipur" // we can give value without using the var or let
+let accName;
+
+console.table([accountId, accountEmail, accPassword, accountCity, accName])
+
+// Remember always use let instead of var because of issues in block and function scope
