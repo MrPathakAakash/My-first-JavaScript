8bce37a758646266673a137590d829264532f9f3
Aakash Pathak <aakashpathakece063@gmail.com>
2025-08-22 01:12:45 +0530

    Added datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // we are using node.js not browser
+
+let name = "Aakash"
+let age = 21
+let isLogic = false
+
+/* Datatypes: numbers, bigint, string, boolean, null(standalone value), undefined,
+symbol (unique) null is an object in typeof
+Object */
+
+console.log(typeof "Aakash")
