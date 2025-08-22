134aab64f626f8cf3ae83818d2ef271cbd51929f
Aakash Pathak <aakashpathakece063@gmail.com>
2025-08-22 12:59:11 +0530

    Operations

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -15,3 +15,22 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn)
 // "" Empty String = false, "some string" = true in boolean
 // Same we have String
+
+// ************************ Operations *****************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Aakash"
+let str3 = str1 + str2
+
+console.log("1" + 2 + 2) // if string first then every thing is string OP = 122
+console.log(1 + 2 + "2") // here its not like above so OP = 32
+
+console.log(+true)
+console.log(+"")
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
