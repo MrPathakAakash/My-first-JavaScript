8aa5528d93244475f905a47182b9558c7ef8cbcd
Aakash Pathak <aakashpathakece063@gmail.com>
2025-08-22 19:26:36 +0530

    Comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,14 @@
+// the basic comparison = < > ! == etc more
+ console.log("2" > 1);
+ console.log("02" > 1);
+ 
+ console.log(null > 0);
+ console.log(null == 0); // equality operator
+ console.log(null >= 0);
+ // the above comparison hum isko avoid karte hai
+ // if there was undefined instead of null it will give false for every comparison above
+
+ // Strict check === it checks the value and the datatype
+ console.log("2" === 2);
+ 
+ 
