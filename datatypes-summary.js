a03b5470bc7c6871203b75f1feab755ff132d957
Aakash Pathak <aakashpathakece063@gmail.com>
2025-08-23 18:02:33 +0530

    DataType Summary

---
+++ datatypes-summary.js
@@ -0,0 +1,27 @@
+// Primitive
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+//JS is an Dynamic language
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+let userEmail; // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123') // creates a unique, hidden identifier mainly used as object keys
+
+const bigNumber = 10000n // n makes it bigInt
+
+// Reference (Non primitive)
+// Array, Objects, Functions
+
+const heros = ["Iron man", "SpiderMan"]
+// Objects in curly braces
+let myObj = {
+    hello: "Aakash"
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world")
+}
